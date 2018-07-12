91172c50c384c447327a1e70a69b6eccd2ba381a
Justin Oswald <justinoswald95@gmail.com>
2018-07-12 09:25:51 -0500

    routing

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import App from './Containers/App.js';
 import {IndexRoute, Router, Route, hasHistory } from 'react-router;'
 export default (
   <Router history={hasHistory}>
